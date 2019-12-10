e24507ee14ee031eeffa2a3d31322ec6f1121ece
e <1792314541@qq.com>
2019-12-10 19:15:28 +0800

    Initial commit

---
+++ src/main/java/yuhao/PenetrationTest/PenetrationTestApplication.java
@@ -0,0 +1,13 @@
+package yuhao.PenetrationTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PenetrationTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PenetrationTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/yuhao/PenetrationTest/PenetrationTestApplicationTests.java
@@ -0,0 +1,13 @@
+package yuhao.PenetrationTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PenetrationTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
